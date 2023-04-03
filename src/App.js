0f4279bc1bd121dfade0ebb5b4b6594f9fae7b1a
Rohan Hangal <rohanhangal@gmail.com>
2023-04-03 12:26:48 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const Login = () => {
+  return <h2>Login</h2>;
+};
+const FriendList = () => {
+  return <h2>Friend List</h2>;
+};
+const AddFriend = () => {
+  return <h2>Add Friend</h2>;
+};
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <Routes>
+        <Route path="/" element={<Login />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/friends" element={<FriendList />} />
+
+        <Route path="/friends/add" element={<AddFriend />} />
+      </Routes>
     </div>
   );
 }
